3013a5ba46c239ef3300aa761579cba2a1055353
Alex Aguilar <alexagui@gmail.com>
2020-08-17 14:34:27 -0400

    Create Asset Volume for Board Game Image

--- config/general.php
+++ config/general.php
@@ -26,6 +26,9 @@ return [
         // Whether to save the project config out to config/project.yaml
         // (see https://docs.craftcms.com/v3/project-config.html)
         'useProjectConfigFile' => false,
+        'aliases' => [
+            '@web' => getenv('DEFAULT_SITE_URL'),
+        ],
     ],
 
     // Dev environment settings
